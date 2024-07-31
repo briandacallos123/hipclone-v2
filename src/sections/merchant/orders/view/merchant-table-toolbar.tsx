4c17969868a66128f42c64d6e02dbf53f31a58ab
brix-apg <briandacallos39@gmail.com>
2024-07-31 23:02:26 +0800

    changes

---
+++ src/sections/merchant/orders/view/merchant-table-toolbar.tsx
@@ -0,0 +1,157 @@
+import { useCallback } from 'react';
+// @mui
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Stack from '@mui/material/Stack';
+import MenuItem from '@mui/material/MenuItem';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import IconButton from '@mui/material/IconButton';
+import FormControl from '@mui/material/FormControl';
+import InputAdornment from '@mui/material/InputAdornment';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+// hooks
+import { useResponsive } from 'src/hooks/use-responsive';
+// types
+import { IAppointmentTableFilters, IAppointmentTableFilterValue } from 'src/types/appointment';
+// components
+import Iconify from 'src/components/iconify';
+import { TableToolbarPopover } from 'src/components/table';
+import CustomPopover, { usePopover } from 'src/components/custom-popover';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  filters: IAppointmentTableFilters;
+  onFilters: (name: string, value: IAppointmentTableFilterValue) => void;
+  //
+  hospitalOptions: string[];
+};
+
+export default function MerchantTableToolbar({
+  filters,
+  onFilters,
+  //
+  hospitalOptions,
+}: Props) {
+  const upMd = useResponsive('up', 'md');
+
+  const popover = usePopover();
+
+  const handleFilterName = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      onFilters('search', event.target.value);
+    },
+    [onFilters]
+  );
+
+  const handleFilterHospital = useCallback(
+    (event: SelectChangeEvent<string[]>) => {
+      onFilters(
+        'hospital',
+        typeof event.target.value === 'string' ? event.target.value.split(',') : event.target.value
+      );
+    },
+    [onFilters]
+  );
+
+  const handleFilterStartDate = useCallback(
+    (newValue: any) => {
+      onFilters('startDate', newValue);
+    },
+    [onFilters]
+  );
+
+  const handleFilterEndDate = useCallback(
+    (newValue: any) => {
+      onFilters('endDate', newValue);
+    },
+    [onFilters]
+  );
+
+  const renderFields = (
+    <>
+      
+
+    </>
+  );
+
+  return (
+    <>
+      <Stack
+        spacing={2}
+        alignItems={{ xs: 'flex-end', md: 'center' }}
+        direction={{
+          xs: 'column',
+          md: 'row',
+        }}
+        sx={{
+          p: 2.5,
+          pr: { xs: 2.5, md: 1 },
+        }}
+      >
+        {upMd && renderFields}
+
+        <Stack direction="row" alignItems="center" spacing={2} flexGrow={1} sx={{ width: 1 }}>
+          <TextField
+            // name="search"
+            fullWidth
+            value={filters.search}
+            onChange={handleFilterName}
+            placeholder="Search Order Id..."
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled' }} />
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          {/* <Stack direction="row">
+            {!upMd && <TableToolbarPopover>{renderFields}</TableToolbarPopover>}
+
+            <IconButton onClick={popover.onOpen}>
+              <Iconify icon="eva:more-vertical-fill" />
+            </IconButton>
+          </Stack> */}
+        </Stack>
+      </Stack>
+
+      {/* <CustomPopover
+        open={popover.open}
+        onClose={popover.onClose}
+        arrow="right-top"
+        sx={{ width: 140 }}
+      >
+        <MenuItem
+          onClick={() => {
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:printer-minimalistic-bold" />
+          Print
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:import-bold" />
+          Import
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:export-bold" />
+          Export
+        </MenuItem>
+      </CustomPopover> */}
+    </>
+  );
+}
